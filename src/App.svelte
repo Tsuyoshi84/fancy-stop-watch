438a57ccf3df5cf056b7505b6ec26aa49ea39f4c
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2020-06-07 23:43:13 +0900

    Add title color

--- src/App.svelte
+++ src/App.svelte
@@ -32,6 +32,19 @@
     padding: 1rem;
     font-size: 3rem;
     display: block;
+    color: transparent;
+
+    background: linear-gradient(90deg, #aaf, #aaf 50%, #fff 0);
+    background-clip: text;
+    -webkit-background-clip: text;
+    background-size: 200% 100%;
+    background-position: 100%;
+    -webkit-text-fill-color: transparent;
+    transition: background-position 275ms ease;
+  }
+
+  h1:hover {
+    background-position: 0 100%;
   }
 
   main {
