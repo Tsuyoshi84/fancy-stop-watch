edd5db15fb743c45cd9385b9d80397846bb2bd62
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2020-09-13 11:12:20 +0900

    Change color

--- src/StartButton.svelte
+++ src/StartButton.svelte
@@ -10,7 +10,7 @@
 
 <style>
   button {
-    --color: blue;
+    --color: #88f;
 
     color: var(--color);
     background-color: rgb(0 0 0 / 0.7);
@@ -19,12 +19,13 @@
     font-size: 5rem;
     padding: 1.5rem;
     transition: all 0.2s ease-in-out;
+    box-shadow: 0 0 20px 0px blue;
   }
 
   button:hover {
     cursor: pointer;
     background-color: rgb(0 0 0 / 0.2);
-    filter: brightness(2);
+    box-shadow: 0 0 40px 10px blue;
   }
 </style>
 
